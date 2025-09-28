18216c0ef3287f821f2987e636c567c16293fb4e
lucaskrapp2543 <yasminpereiramm@gmail.com>
2025-09-28 12:26:30 -0300

    instalaçoes pixeis

--- src/pages/ConhecerV3.tsx
+++ src/pages/ConhecerV3.tsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+// Meta Pixel Code
+declare global {
+  interface Window {
+    fbq: any;
+  }
+}
+
 interface QuizState {
   step: number;
   businessType: string | null;
@@ -123,6 +130,27 @@ const ConhecerV3 = () => {
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [quizState.step]);
+
+  // Meta Pixel - Carregar pixel do Facebook
+  useEffect(() => {
+    // Carregar script do Facebook Pixel
+    if (!window.fbq) {
+      const script = document.createElement('script');
+      script.innerHTML = `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '760382943505536');
+        fbq('track', 'PageView');
+      `;
+      document.head.appendChild(script);
+    }
+  }, []);
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col">
